20f9d9911ad1b84108beb32bb79871c4ae12b615
L_Tryharder <150335774+Lorenzo101107@users.noreply.github.com>
2024-03-05 09:21:34 +0100

    05-03.c
    si

--- 05-3.c
+++ 05-3.c
@@ -1,13 +1,20 @@
-/*Dopo aver caricato in memoria un 2 vettori di uguale dimensione composti da numeri interi
-scrivere in output un terzo vettore dato dalla somma delle componenti fatta a una a una (Vettore
-somma).*/
+//Dopo aver dichiarato un vettore di numeri interi 
+//Leggere in imput gli elementi 
+//e sommarli visualizzando il risultato 
 
 #include <stdio.h>
 int main()
 {
-    int lunghezza;
-    printf("Inserisci la lunghezza dei vettori");
-    scanf("%d",&lunghezza);
+    int somma=0, vettore[10], i, n;
+    do{
+    printf("Quanti elementi vuoi inserire? (max 10)\n");
+    scanf("%d",&n);
+    }while(n<=0);
+    for(i=0;i<n;i++){
+        printf("Inserisci gli elemeti %d",i+1);
+        scanf("%d",&vettore[i]);
+        somma=somma+vettore[i];
+    }
+    printf("La somma e' %d", somma);
     return 0;
-    
 }
