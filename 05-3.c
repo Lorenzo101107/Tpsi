e632228b44153037be26bca9c37f75910689bb0a
L_Tryharder <150335774+Lorenzo101107@users.noreply.github.com>
2024-03-05 09:07:49 +0100

    05-3.c
    ok

---
+++ 05-3.c
@@ -0,0 +1,13 @@
+/*Dopo aver caricato in memoria un 2 vettori di uguale dimensione composti da numeri interi
+scrivere in output un terzo vettore dato dalla somma delle componenti fatta a una a una (Vettore
+somma).*/
+
+#include <stdio.h>
+int main()
+{
+    int lunghezza;
+    printf("Inserisci la lunghezza dei vettori");
+    scanf("%d",&lunghezza);
+    return 0;
+    
+}
